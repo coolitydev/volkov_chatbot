5a1d61618954e291eabb2ac99079fc82e71b418c
coolity <coolitydev@gmail.com>
2024-06-09 17:23:05 +0300

    Create main

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config import settings
+
+
+async def main() -> None:
+    bot = Bot(token=settings.bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher()
+
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
